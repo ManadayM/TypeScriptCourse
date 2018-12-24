47aa906203e50312e564f46510fc335f7b57230d
Manaday Mavani <manaday.mavani@investisdigital.com>
2018-12-24 12:50:33 +0000

    Explicit types

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // string
 var myName = 'Manaday';
-myName = 28;
+// myName = 28;
 // number
 var myAge = 27.5;
-myAge = 'twenty seven';
+// myAge = 'twenty seven';
 // boolean
 var hasHobbies = false;
-hasHobbies = 1;
-// this will show compile time errors
+// hasHobbies = 1;
+// this will work as for undefined variables typescript sets implicit type to "any"
+var myRealAge1;
+myRealAge1 = 27;
+myRealAge1 = '27';
+// this is explicit type
+var myRealAge2;
+myRealAge2 = 27;
+myRealAge2 = '27';
--- app.ts
+++ app.ts
@@ -1,13 +1,21 @@
 // string
 let myName = 'Manaday';
-myName = 28;
+// myName = 28;
 
 // number
 let myAge = 27.5;
-myAge = 'twenty seven';
+// myAge = 'twenty seven';
 
 // boolean
 let hasHobbies = false;
-hasHobbies = 1;
+// hasHobbies = 1;
 
-// this will show compile time errors
+// this will work as for undefined variables typescript sets implicit type to "any"
+let myRealAge1;
+myRealAge1 = 27;
+myRealAge1 = '27';
+
+// this is explicit type
+let myRealAge2: number;
+myRealAge2 = 27;
+myRealAge2 = '27';
